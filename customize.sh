9fe6c353cf8fdf45ad2b9d1af5aa9a02c4f20119
Noir <134027541+Zy0x@users.noreply.github.com>
2025-03-25 20:01:19 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -19,13 +19,13 @@ if [[ '$stock_path' ]] ; then umount -l $stock_path; fi
 
 # Install YoutubeRVX
 ui_print "Installing Stock Youtube..."
-SESSION=$(pm install-create -r | grep -oE '[0-9]+')
+SESSION=$(pm install-create -d | grep -oE '[0-9]+')
 APKS="$(ls $MODPATH/youtube)"
 for APK in $APKS; do
 pm install-write $SESSION $APK $MODPATH/youtube/$APK > /dev/null
 done
 pm install-commit $SESSION
-rm -rf $MODPATH/youtube
+mv $MODPATH/youtube $MODPATH/ori/
 
 if ! dumpsys package $PKGNAME | grep path > /dev/null 2>&1
 then
@@ -33,7 +33,6 @@ then
 	ui_print "- Install $APPNAME from PlayStore"
 	abort "- Aborting installation !!"
 fi
-
 
 STOCKAPPVER=$(dumpsys package $PKGNAME | grep versionName | cut -d= -f 2 | sed -n '1p')
 RVAPPVER=$(grep version= module.prop | sed 's/version=v//')
@@ -66,6 +65,16 @@ chcon u:object_r:apk_data_file:s0 "$RVAPK"
 mount -o bind "$RVAPK" "$STOCKAPK"
 am force-stop "$PKGNAME"
 
+# Detect Root
+ui_print "- Detect FullRoot/TempRoot..."
+if [ -e /data/local/tmp/magisk ]; then
+    ui_print "Temproot Detected!"
+    ui_print "Make configuration..."
+    sed -i '/#wait_until_login/s/.*/wait_until_login/' $MODPATH/service.sh
+else
+    ui_print "Full Root Detected!"
+fi
+
 rm -rf /data/adb/revanced
 rm -rf /data/adb/service.d/*revanced*.sh
 rm -rf /data/adb/service.d/detach*.sh
@@ -75,7 +84,9 @@ rm -rf /data/adb/post-fs-data.d/detach*.sh
 rm -rf /data/adb/post-fs-data.d/*youtube*.sh
 rm -rf /data/local/tmp/revanced*
 
-
 ui_print "- Install Successful !!"
 
 ui_print "- You can use YouTube ReVanced-Extended without reboot as well."
+ui_print "- Try to open YouTube (in 3s)..."
+sleep 3
+am start -n com.google.android.youtube/com.google.android.apps.youtube.app.WatchWhileActivity
